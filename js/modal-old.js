08f94aae87e4ad224383115c28cbe2eac6f9a448
Al Tsaplin <al.tsaplin@gmail.com>
2023-08-05 22:04:31 +0300

    add(search-modal)unfinished. Needs solving an error.

--- js/modal-old.js
+++ js/modal-old.js
@@ -7,8 +7,8 @@ const modal = () => {
   const modal = document.querySelector('.search-modal'),
     modalButton = document.querySelector('.icon_search'),
     modalCloseButton = modal.querySelector('.search-close-switch'),
-    searchInput = modal.querySelector('#search-input');
-  // wrapper = modal.querySelector('.search-modal-result');
+    searchInput = modal.querySelector('#search-input'),
+    wrapper = modal.querySelector('.search-modal-result');
 
   // Полученный массив будем отправлять на отрисовку\рендер. Туда нам нужно также получить обёртку, тот блок, в который мы будем класть элементы перебором.
   const renderFunc = (items) => {
@@ -25,31 +25,18 @@ const modal = () => {
   };
   // В данную функцию мы будем передавать value нашей строки ввода поиска. К массиву данных применяем метод filter(), при каждой итерации извлекаем из него dataItem, а возвращать только те элементы dataItem, которые подходят к запросу поиска. Т.е. есть ли буквы из строки ввода в свойствах "title" & "description".
   // Чтобы получалось находить запросы любым регистром используем метод toLowerCase().
-  // const searchFunc = (searchStr) => {
-  //   fetch('./db.json')
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       const filteredData = data.filter((dataItem) => {
-  //         return (
-  //           dataItem.title.toLowerCase().includes(searchStr.toLowerCase()) ||
-  //           dataItem.description.toLowerCase().includes(searchStr.toLowerCase())
-  //         );
-  //       });
-
-  //       renderFunc(filteredData.slice(0, 5));
-  //     });
-  // };
-
   const searchFunc = (searchStr) => {
     fetch('./db.json')
-      .then((response) => {
-        return response.json();
-      })
+      .then((response) => response.json())
       .then((data) => {
-        data.filter((dataItem) => {
-          console.log(dataItem);
-          return dataItem;
+        const filteredData = data.filter((dataItem) => {
+          return (
+            dataItem.title.toLowerCase().includes(searchStr.toLowerCase()) ||
+            dataItem.description.toLowerCase().includes(searchStr.toLowerCase())
+          );
         });
+
+        renderFunc(filteredData.slice(0, 5));
       });
   };
 
@@ -64,7 +51,6 @@ const modal = () => {
     wrapper.innerHTML = '';
   });
   // При вводе информации в строку ввода нам необходимо делать запрос
-  // searchInput.addEventListener('input', (evt) => searchFunc(evt.target.value));
   searchInput.addEventListener('input', (evt) => searchFunc(evt.target.value));
 };
 
